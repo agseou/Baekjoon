d401aa405461713380cc31e7817f5588e3951395
떠우 <108851660+agseou@users.noreply.github.com>
2023-07-20 09:24:40 +0900

    [Gold V] Title: 탑, Time: 104 ms, Memory: 6300 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+stack<pair<int,int>> s;
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int N, input;
+  cin >> N;
+  s.push({100000001,0});
+  for(int i = 1; i <= N; i++){
+    cin >> input;
+    while(input > s.top().first){
+      s.pop();
+    }
+    cout << s.top().second << ' ';
+    s.push({input,i});
+  }
+}
