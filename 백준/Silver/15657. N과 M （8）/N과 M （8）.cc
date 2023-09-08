6bb604218e7b3f8d471afa38d948e5b8a3dabe54
SeoU <108851660+agseou@users.noreply.github.com>
2023-09-09 02:55:14 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int num[10];
+int arr[10]; // 수열을 담을 배열
+
+void func(int K){
+  if(K == M){ // M개를 모두 선택 -> 출력!
+    for(int i = 0; i < M; i++)
+      cout << num[arr[i]] << ' ';
+    cout << '\n';
+    return;
+  }
+  // 배열 채우기
+  for(int i = arr[K-1]; i < N; i++){
+    arr[K] = i;
+    func(K+1);
+  }
+}
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N >> M;
+  for(int i = 0; i < N; i++)
+    cin >> num[i];
+  sort(num, num+N);
+  func(0); //재귀 이용
+}
