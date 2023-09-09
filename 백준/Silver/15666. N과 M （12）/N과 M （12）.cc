f1b230fcb2d6524debca780bbf61c3cc0bd91ae6
SeoU <108851660+agseou@users.noreply.github.com>
2023-09-09 23:47:32 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int num[10];
+int arr[10]; // 수열을 담을 배열
+
+void func(int K, int st){
+  if(K == M){ // M개를 모두 선택 -> 출력!
+    for(int i = 0; i < M; i++)
+      cout << arr[i] << ' ';
+    cout << '\n';
+    return;
+  }
+  int tmp = 0;
+  for(int i = st; i < N; i++){
+    if(tmp == num[i]) continue;
+    arr[K] = num[i];
+    tmp = arr[K];
+    func(K+1, i);
+  }
+}
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N >> M;
+  for(int i = 0; i < N; i++)
+    cin >> num[i];
+  sort(num, num+N);
+  func(0, 0); //재귀 이용
+}
